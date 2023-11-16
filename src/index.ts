e06ef4c2ec86e4b445fb35213002c82845a36b29
OtavioRF <otavio.felippi@estudante.ifms.edu.br>
2023-11-16 09:53:08 -0400

    Metodo post

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import express from 'express'
 import ListaFilme from './aplicacao/lista_filme.use-case'
 import BancoMongoDB from './infra/banco/banco_mongodb'
 import BancoEmMemoria from './infra/banco/banco_em_memoria'
+import SalvaFilme from './aplicacao/salva_filme.use-case'
 const app = express()
+const bancoMongoDB = new BancoMongoDB()
 app.use(express.json())
 //Tenho que ter uma rota post para cadastrar um filme
 type Filme = {
@@ -12,7 +14,7 @@ type Filme = {
     imagem:string
 }
 let filmesCadastrados:Filme[] = []
-app.post('/filmes',(req,res)=>{
+app.post('/filmes', async(req,res)=>{
     const {id, titulo, descricao, imagem} = req.body
     const filme = {
         id,
@@ -21,12 +23,13 @@ app.post('/filmes',(req,res)=>{
         imagem
     }
     //como salvo o filme
-    filmesCadastrados.push(filme)
-    res.status(201).send(filme)
+    const salvarFilme = new SalvaFilme(bancoMongoDB)
+    const filmes = await salvarFilme.execute(filme)
+    // filmesCadastrados.push(filme)
+    res.status(201).send(filmes)
 })
 app.get('/filmes', async(req,res)=>{
     //usem o listarFilme para listar os filmes
-    const bancoMongoDB = new BancoMongoDB()
     const listaFilme = new ListaFilme(bancoMongoDB)
     const filmes = await listaFilme.executar()
     res.send(filmes)
